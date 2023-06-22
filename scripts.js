3a870615fac5d26e298839c64c59b050318c0511
sibi361 <104310887+sibi361@users.noreply.github.com>
2023-06-22 21:35:11 +0530

    Improve Navigation Backend For Real-World Applications
    Modified the `changeViewerImage()` function to obtain the `src` of the
    next/previous image in the gallery and set it as the `src` for the
    viewer image

--- scripts.js
+++ scripts.js
@@ -1,30 +1,31 @@
+const IMAGE_COUNT = 50;
+const IMAGES_DIRECTORY = "images/";
+const IMAGES_EXTENSION = ".jpg";
+
 const bodyElement = document.querySelector("body");
 const gallery = document.querySelector(".gallery");
 const overlay = document.querySelector(".overlay");
 const viewer = document.querySelector(".viewer");
 const viewerImage = document.querySelector(".viewerImage");
-
-const IMAGE_COUNT = 50;
-const IMAGES_DIRECTORY = "images/";
-const IMAGES_EXTENSION = ".jpg";
+const navElement = document.querySelector(".nav");
 
 function showViewer() {
     overlay.style.display = "block";
     bodyElement.style.overflow = "hidden";
+
     viewerImage.src = this.src;
+    this.classList.add("viewing");
 }
 
 function hideViewer() {
     overlay.style.display = "none";
     bodyElement.style.overflow = "visible";
+
+    const lastImage = document.querySelector(".viewing");
+    lastImage.classList.remove("viewing");
 }
 
-function createImageLink(id) {
-    return IMAGES_DIRECTORY + String(id) + IMAGES_EXTENSION;
-}
-
-function addImageToGallery(id) {
-    const imageLink = createImageLink(id + 1);
+function addImageToGallery(imageLink) {
     const image = document.createElement("img");
     image.className = "galleryImage";
     image.setAttribute("loading", "lazy");
@@ -33,46 +34,38 @@ function addImageToGallery(id) {
     gallery.append(image);
 }
 
-for (let index = 0; index < IMAGE_COUNT; index++) {
-    addImageToGallery(index);
+// adding sample images to gallery
+for (let index = 1; index <= IMAGE_COUNT; index++) {
+    const sampleImageLink = `${IMAGES_DIRECTORY}${String(
+        index
+    )}${IMAGES_EXTENSION}`;
+    addImageToGallery(sampleImageLink);
 }
-
-overlay.addEventListener("click", hideViewer);
 
 // viewer nav
 const navBackward = document.querySelector(".nav-backward");
 const navSpacer = document.querySelector(".nav-spacer");
 const navForward = document.querySelector(".nav-forward");
 
-function getImageIdFromLink(imgLink) {
-    return Number(
-        imgLink
-            .replace(document.location.toString(), "")
-            .replace(IMAGES_DIRECTORY, "")
-            .replace(IMAGES_EXTENSION, "")
-    );
-}
 function changeViewerImage(delta) {
-    const currentImageId = getImageIdFromLink(viewerImage.src);
-    let newImageId = currentImageId + delta;
-    if (newImageId < 1) newImageId = IMAGE_COUNT + newImageId;
-    if (newImageId > IMAGE_COUNT) newImageId -= IMAGE_COUNT;
-    viewerImage.src = createImageLink(newImageId);
+    const currentImage = document.querySelector(".viewing");
+    if (delta === -1 || delta === 1) {
+        let nextImage;
+        if (delta === -1) {
+            nextImage = currentImage.previousElementSibling;
+            if (nextImage === null || nextImage === undefined)
+                nextImage = currentImage.parentNode.lastChild;
+            viewerImage.src = nextImage.src;
+        } else if (delta === 1) {
+            nextImage = currentImage.nextElementSibling;
+            if (nextImage === null || nextImage === undefined)
+                nextImage = currentImage.parentNode.firstChild;
+        }
+        viewerImage.src = nextImage.src;
+        currentImage.classList.remove("viewing");
+        nextImage.classList.add("viewing");
+    }
 }
-
-navBackward.addEventListener("click", (e) => {
-    e.stopPropagation();
-    changeViewerImage(-1);
-});
-
-navForward.addEventListener("click", (e) => {
-    e.stopPropagation();
-    changeViewerImage(1);
-});
-
-navSpacer.addEventListener("click", (e) => {
-    e.stopPropagation();
-});
 
 document.addEventListener("keydown", (e) => {
     const key = e.key.toLowerCase();
@@ -81,3 +74,13 @@ document.addEventListener("keydown", (e) => {
 
     if (key === "escape") hideViewer();
 });
+
+navBackward.addEventListener("click", () => changeViewerImage(-1));
+
+navForward.addEventListener("click", () => changeViewerImage(1));
+
+overlay.addEventListener("click", hideViewer);
+
+// masks the above listener just in case the user
+// clicks on the viewer image
+navElement.addEventListener("click", (e) => e.stopPropagation());
